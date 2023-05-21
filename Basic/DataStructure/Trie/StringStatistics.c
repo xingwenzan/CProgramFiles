b8a250d3d489536e63763e385037788246c0c21f
86159 <zxw3180716935@163.com>
2023-05-21 15:08:14 +0800

    add

--- Basic/DataStructure/Trie/StringStatistics.c
+++ Basic/DataStructure/Trie/StringStatistics.c
@@ -1,3 +1,43 @@
 //
 // Created by 86159 on 2023-05-21.
 //
+
+#include "stdio.h"
+
+#define N 100010
+
+int son[N][26], cnt[N], idx = 0;
+char str[N];
+
+void insert(char *string) {
+    int pointer = 0;
+    for (int i = 0; string[i]; ++i) {
+        int letter = string[i] - 'a';
+        if (!son[pointer][letter]) { son[pointer][letter] = ++idx; }
+        pointer = son[pointer][letter];
+    }
+    cnt[pointer]++;
+}
+
+int query(char *string) {
+    int pointer = 0;
+    for (int i = 0; string[i]; ++i) {
+        int letter = string[i] - 'a';
+        if (!son[pointer][letter]) { return 0; }
+        pointer = son[pointer][letter];
+    }
+    return cnt[pointer];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        char op[2];
+        scanf("%s%s", op, str);
+        if (*op == 'I') insert(str);
+        else printf("%d\n", query(str));
+    }
+
+    return 0;
+}
